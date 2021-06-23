c090e0dde52e308c98b9d8247c28f3aba7296546
신예린 <shin0411a@naver.com>
2021-06-23 18:21:19 +0900

    0623

---
+++ Assets/Scripts/effect.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameTextControl : MonoBehaviour {
+    public GameObject readText;
+    public static GameTextControl instance;
+
+
+    void Awake()
+    {
+        if (GameTextControl.instance == null)
+            GameTextControl.instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        readText.SetActive(false);
+        StartCoroutine(ShowReady());
+    }
+
+    IEnumerator ShowReady()
+    {
+        int count = 0;
+        while (count < 3)
+        {
+            readText.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            readText.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            count++;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
